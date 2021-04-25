10ad2b8c90b7df91727cc47219f9ef20438d05e2
Hedayat <hedayat94@gmail.com>
2021-04-25 14:50:32 +0100

    changing RelationMap: pid's string id as key & InternalPID as the value

--- internal/relations/relations.go
+++ internal/relations/relations.go
@@ -6,7 +6,7 @@ import (
 )
 
 type RelationType int32
-type RelationMap map[p.InternalPID]struct{}
+type RelationMap map[string]p.InternalPID
 
 const (
 	LinkedRelation RelationType = iota
@@ -32,9 +32,9 @@ func NewRelation() *Relations {
 func (r *Relations) RelationType(pid p.InternalPID) RelationType {
 	r.RLock()
 	defer r.RUnlock()
-	if _, ok := r.linkedActors[pid]; ok {
+	if _, ok := r.linkedActors[pid.ID()]; ok {
 		return LinkedRelation
-	} else if _, ok := r.monitoredActors[pid]; ok {
+	} else if _, ok := r.monitoredActors[pid.ID()]; ok {
 		return MonitoredRelation
 	}
 	return NoRelation
@@ -43,37 +43,37 @@ func (r *Relations) RelationType(pid p.InternalPID) RelationType {
 func (r *Relations) AddLink(to p.InternalPID) {
 	r.Lock()
 	defer r.Unlock()
-	r.linkedActors[to] = struct{}{}
+	r.linkedActors[to.ID()] = to
 }
 
 func (r *Relations) RemoveLink(from p.InternalPID) {
 	r.Lock()
 	defer r.Unlock()
-	delete(r.linkedActors, from)
+	delete(r.linkedActors, from.ID())
 }
 
 func (r *Relations) AddMonitored(who p.InternalPID) {
 	r.Lock()
 	defer r.Unlock()
-	r.monitoredActors[who] = struct{}{}
+	r.monitoredActors[who.ID()] = who
 }
 
 func (r *Relations) RemoveMonitored(who p.InternalPID) {
 	r.Lock()
 	defer r.Unlock()
-	delete(r.monitoredActors, who)
+	delete(r.monitoredActors, who.ID())
 }
 
 func (r *Relations) AddMonitor(by p.InternalPID) {
 	r.Lock()
 	defer r.Unlock()
-	r.monitorActors[by] = struct{}{}
+	r.monitorActors[by.ID()] = by
 }
 
 func (r *Relations) RemoveMonitor(by p.InternalPID) {
 	r.Lock()
 	defer r.Unlock()
-	delete(r.monitorActors, by)
+	delete(r.monitorActors, by.ID())
 }
 
 func (r *Relations) LinkedActors() *RelationIterator {
@@ -81,7 +81,7 @@ func (r *Relations) LinkedActors() *RelationIterator {
 	defer r.RUnlock()
 
 	linkedActors := make([]p.InternalPID, 0, len(r.linkedActors))
-	for pid := range r.linkedActors {
+	for _, pid := range r.linkedActors {
 		linkedActors = append(linkedActors, pid)
 	}
 
@@ -98,7 +98,7 @@ func (r *Relations) MonitorActors() *RelationIterator {
 	defer r.RUnlock()
 
 	monitorActors := make([]p.InternalPID, 0, len(r.monitorActors))
-	for pid := range r.monitorActors {
+	for _, pid := range r.monitorActors {
 		monitorActors = append(monitorActors, pid)
 	}
 
