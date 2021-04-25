e570a95b297bbdb74e4887067b6366afb1437955
Hedayat <hedayat94@gmail.com>
2021-04-25 16:34:30 +0100

    add test for public pid

--- pid/pid_test.go
+++ pid/pid_test.go
@@ -1,1 +1,26 @@
 package pid
+
+import (
+	"github.com/hedisam/goactor/internal/intlpid"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestToPID(t *testing.T) {
+	internalPID := intlpid.NewMockInternalPID()
+
+	pid := ToPID(internalPID)
+	if !assert.NotNil(t, pid) {return}
+	if !assert.Equal(t, internalPID, pid.InternalPID()) {return}
+	assert.Equal(t, internalPID.ID(), pid.ID())
+	assert.Equal(t, internalPID.IsSupervisor(), pid.IsSupervisor())
+
+	pid2 := ToPID(internalPID)
+	if !assert.NotNil(t, pid2) {return}
+	assert.Equal(t, pid, pid2)
+
+	internalPID2 := intlpid.NewMockInternalPID()
+	pid3 := ToPID(internalPID2)
+	assert.NotEqual(t, pid3, pid)
+
+}
